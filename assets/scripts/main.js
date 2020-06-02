d18d1fd3673e7657b9d0ed628bf99585e825c9cd
Nicole Graiff <nicole.graiff@gmail.com>
2020-06-02 18:25:03 -0400

    Added assets folder containing starter css and js files.

---
+++ assets/scripts/main.js
@@ -0,0 +1,123 @@
+
+
+
+/*
+//Set up all variables
+var score = 0;
+
+var time = 0;
+
+//
+var highScore = document.getElementById("high-score"); // on click reveal high scores screen
+
+var startContainer = document.getElementById("main-container");
+
+var start = document.getElementById("start"); // on click start quiz
+
+var questionContainer = document.getElementById("question-container"); //screen is revealed when the start button is clicked from the start screen
+
+var timer = document.getElementById("timer");
+
+var timeLeft = 75;
+
+var question = document.getElementById("question");
+
+var answerA = document.getElementById("answerA");
+
+var answerB = document.getElementById("answerB");
+
+var answerC = document.getElementById("answerC");
+
+var answerD = document.getElementById("answerD");
+
+var resultContainer = document.getElementById("quiz-results"); // at end of quiz, reveals the results screen
+
+var finalScore = document.getElementById("final-score"); // at end of quiz, displays the final score
+
+var scoreBoard = document.getElementById("scores");
+
+
+//Countdown clock function
+var countdownClock = setInterval(function() {
+    timer.textContent = timeLeft;
+    timeLeft--;
+
+    if (timeLeft === 0) {
+      timer.textContent = "0";
+      clearInterval(timeInterval);
+      questionContainer.style.display = "none";
+      resultContainer.style.display = "block"
+    }
+
+  }, 1000);
+
+//Navigate to high score page
+highScore.onclick = fuction(){
+    scoreBoard.style.display = "block";
+    startContainer.style.display = "none";
+    questionContainer.style.display = "none";
+    resultContainer.style.display = "none";
+};
+
+//Start quiz
+start.onclick = function(){
+    startContainer.style.display = "none";
+};
+
+function reveal
+
+
+var quizQuestions = [
+    {
+        question : "Question one?",
+        choiceA : "Option one",
+        choiceB : "Option two",
+        choiceC : "option three",
+        choiceD : "option four",
+        correctAnswer : "B",
+    },
+    {
+        question : "Question two?",
+        choiceA : "Option one",
+        choiceB : "Option two",
+        choiceC : "option three",
+        choiceD : "option four",
+        correctAnswer : "D",
+    },
+    {
+        question : "Question three?",
+        choiceA : "Option one",
+        choiceB : "Option two",
+        choiceC : "option three",
+        choiceD : "option four",
+        correctAnswer : "A",
+    },
+    {
+        question : "Question four?",
+        choiceA : "Option one",
+        choiceB : "Option two",
+        choiceC : "option three",
+        choiceD : "option four",
+        correctAnswer : "C",
+    },
+    {
+        question : "Question five?",
+        choiceA : "Option one",
+        choiceB : "Option two",
+        choiceC : "option three",
+        choiceD : "option four",
+        correctAnswer : "A",
+    }
+];
+
+function correctAnswer(){
+    document.getElementById("answer-check").textContent
+}
+
+
+var populateQuestions = function(){
+    for (var i = 0; i < quizQuestions.length; i++){
+
+    }
+}
+*/
