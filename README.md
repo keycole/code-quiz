2c8a0f9c14d412cb77e6033690bac1b2ffa665d0
Nicole Graiff <nicole.graiff@gmail.com>
2020-06-03 07:28:23 -0400

    Added screenshots to readme file to show what the quiz flow should look like when functional.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # code-quiz
 A timed code quiz with multiple-choice questions. This app runs in the browser and features dynamically updated HTML and CSS powered by JavaScript code. It also features a clean and polished user interface. The app is responsive, ensuring that it adapts to multiple screen sizes.
+
+When functioning, the quiz flow should look like this. 
+(Screenshots for mobile and tablet devices will be included when the app is functional).
+
+## Start Screen
+![Start screen](assets/images/readme-images/quizDeskStart.png)
+
+## Questions Screen
+![Question screen](assets/images/readme-images/quizDeskQuestionA.png)
+
+## Answer Check Screen
+![Question screen with result](assets/images/readme-images/quizDeskQuestionB.png)
+
+## Results Screen
+![Results screen](assets/images/readme-images/quizResults.png)
+
+## High Score Screen
+![High score screen](assets/images/readme-images/quizHighScore.png)
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,123 +1,46 @@
-
-
-
-/*
-//Set up all variables
-var score = 0;
-
-var time = 0;
-
-//
-var scoreLink = document.getElementById("score-link"); // on click reveal high scores screen
-
-var startContainer = document.getElementById("main-container");
-
-var start = document.getElementById("start"); // on click start quiz
-
-var questionContainer = document.getElementById("question-container"); //screen is revealed when the start button is clicked from the start screen
-
+//Identify all variables
+var navBar = document.getElementById("navigation-bar");
 var timer = document.getElementById("timer");
+//Container variables
+var quizContainer = document.getElementById("quiz-container");
+var cardHeader = document.getElementById("header");
+var subHead = document.getElementById("sub-text");
+var buttons = document.getElementById("buttons")
+var start = document.getElementById("start");
+//Questions container
+var questionContainer = document.getElementById("question-container");
+var questions = document.getElementById("question");
+var answersA = document.getElementById("answerA");
+var answersB = document.getElementById("answerB");
+var answersC = document.getElementById("answerC");
+var answersD = document.getElementById("answerD");
+var answerCheck = document.getElementById("answer-check");
+var
+//Results container
+var resultsContainer = document.getElementById("quiz-results");
+var finalScore = document.getElementById("final-score");
+var nameField  = document.getElementById("inlineFormInput");
+var submitScore = document.getElementById("submit-score");
+//High Score container
+var highScoreContainer = document.getElementById("high-score");
+var userRecord = document.getElementById("user-name");
+var backButton = document.getElementById("back-button");
+var clear = document.getElementById("clear-scores");
 
-var timeLeft = 75;
+// Time 
+var time;
 
-var question = document.getElementById("question");
-
-var answerA = document.getElementById("answerA");
-
-var answerB = document.getElementById("answerB");
-
-var answerC = document.getElementById("answerC");
-
-var answerD = document.getElementById("answerD");
-
-var resultContainer = document.getElementById("quiz-results"); // at end of quiz, reveals the results screen
-
-var finalScore = document.getElementById("final-score"); // at end of quiz, displays the final score
-
-var scoreBoard = document.getElementById("scores");
-
-
-//Countdown clock function
-var countdownClock = setInterval(function() {
-    timer.textContent = timeLeft;
-    timeLeft--;
-
-    if (timeLeft === 0) {
-      timer.textContent = "0";
-      clearInterval(timeInterval);
-      questionContainer.style.display = "none";
-      resultContainer.style.display = "block"
-    }
-
-  }, 1000);
-
-//Navigate to high score page
-scoreLink.onclick = fuction(){
-    scoreBoard.style.display = "block";
-    startContainer.style.display = "none";
-    questionContainer.style.display = "none";
-    resultContainer.style.display = "none";
-};
+timer.textContent = time;
 
 //Start quiz
-start.onclick = function(){
-    startContainer.style.display = "none";
+
+start.addEventListener("click",  startTimer());
+
+function startTimer () {
+    time = 75;
+    setInterval(function(){
+        time--;
+    } if (time === 0) {
+        clearInterval(startTimer);
+    }
 };
-
-function reveal
-
-
-var quizQuestions = [
-    {
-        question : "Question one?",
-        choiceA : "Option one",
-        choiceB : "Option two",
-        choiceC : "option three",
-        choiceD : "option four",
-        correctAnswer : "B",
-    },
-    {
-        question : "Question two?",
-        choiceA : "Option one",
-        choiceB : "Option two",
-        choiceC : "option three",
-        choiceD : "option four",
-        correctAnswer : "D",
-    },
-    {
-        question : "Question three?",
-        choiceA : "Option one",
-        choiceB : "Option two",
-        choiceC : "option three",
-        choiceD : "option four",
-        correctAnswer : "A",
-    },
-    {
-        question : "Question four?",
-        choiceA : "Option one",
-        choiceB : "Option two",
-        choiceC : "option three",
-        choiceD : "option four",
-        correctAnswer : "C",
-    },
-    {
-        question : "Question five?",
-        choiceA : "Option one",
-        choiceB : "Option two",
-        choiceC : "option three",
-        choiceD : "option four",
-        correctAnswer : "A",
-    }
-];
-
-function correctAnswer(){
-    document.getElementById("answer-check").textContent
-}
-
-
-var populateQuestions = function(){
-    for (var i = 0; i < quizQuestions.length; i++){
-
-    }
-}
-*/
